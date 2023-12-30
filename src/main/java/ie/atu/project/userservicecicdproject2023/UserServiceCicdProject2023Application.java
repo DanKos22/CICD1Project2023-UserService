5ccf61ac5081ea7646a5ae8c760f8be507dc75d1
DanKos22 <G00397054@atu.ie>
2023-12-30 12:43:43 +0200

    Started project

---
+++ src/main/java/ie/atu/project/userservicecicdproject2023/UserServiceCicdProject2023Application.java
@@ -0,0 +1,13 @@
+package ie.atu.project.userservicecicdproject2023;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class UserServiceCicdProject2023Application {
+
+    public static void main(String[] args) {
+        SpringApplication.run(UserServiceCicdProject2023Application.class, args);
+    }
+
+}
---
+++ src/test/java/ie/atu/project/userservicecicdproject2023/UserServiceCicdProject2023ApplicationTests.java
@@ -0,0 +1,13 @@
+package ie.atu.project.userservicecicdproject2023;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class UserServiceCicdProject2023ApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
