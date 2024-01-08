10ca8be6a953fab460348b2168c9b4e5baabd37a
DanKos22 <G00397054@atu.ie>
2024-01-08 16:10:49 +0200

    added some code

--- src/main/java/ie/atu/project/userservicecicdproject2023/UserServiceCicdProject2023Application.java
+++ src/main/java/ie/atu/project/userservicecicdproject2023/UserServiceCicdProject2023Application.java
@@ -2,7 +2,9 @@ package ie.atu.project.userservicecicdproject2023;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.openfeign.EnableFeignClients;
 
+@EnableFeignClients
 @SpringBootApplication
 public class UserServiceCicdProject2023Application {
 
