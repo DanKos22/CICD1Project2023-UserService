be6261058d6abaf4a5d3c956b1d6c9f3eff6fa65
DanKos22 <G00397054@atu.ie>
2024-01-09 20:30:36 +0000

    Did some work on testing

---
+++ src/test/java/ie/atu/project/PersonalDetailsUnitTest.java
@@ -0,0 +1,70 @@
+package ie.atu.project;
+
+import org.junit.jupiter.api.Test;
+import static org.junit.jupiter.api.Assertions.*;
+
+public class PersonalDetailsUnitTest {
+
+    @Test
+    public void NoArgsConstructorTest() {
+        PersonDetails personDetails = new PersonDetails();
+
+        assertNotNull(personDetails);
+        assertNotNull(personDetails.getName());
+        assertNotNull(personDetails.getEmail());
+    }
+
+    @Test
+    public void AllArgsConstructorTest() {
+        String name = "John Doe";
+        String email = "john@atu.ie";
+
+        PersonDetails personDetails = new PersonDetails(name, email);
+        assertNotNull(personDetails);
+        assertEquals(name, personDetails.getName());
+        assertEquals(email, personDetails.getEmail());
+    }
+
+    @Test
+    public void GetterAndSetterTest() {
+
+        PersonDetails personDetails = new PersonDetails();
+        String name = "John Doe";
+        String email = "john@atu.ie";
+
+        personDetails.setName(name);
+        personDetails.setEmail(email);
+
+        assertEquals("John Doe", personDetails.getName());
+        assertEquals(email, personDetails.getEmail());
+
+    }
+
+    public class PersonDetails {
+        String name;
+        String email;
+
+        public PersonDetails(String name, String email) {
+        }
+
+        public PersonDetails() {
+
+        }
+
+        public void setName(String name) {
+            this.name = name;
+        }
+
+        public String getName() {
+            return name;
+        }
+
+        public void setEmail(String email) {
+            this.email = email;
+        }
+
+        public String getEmail() {
+            return email;
+        }
+    }
+}
